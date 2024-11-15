26bbf9d8842d98694d5b4e227c3a15d577a6ae1f
Immanuel Joy <79083453+ImmanuelJoya@users.noreply.github.com>
2024-11-15 02:41:38 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Front Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body style="background-color: black;">
+
+    <a href="list.php"> <button id="logInButton">Subscriptions</button> </a>
+    <a href="index.php"> <button id="signUpButton">Home</button></a>
+
+    <header>
+        <div class="overlay">
+            <section class="hero">
+                <img src="AAM.svg" alt="" class="icons">
+                <img src="AM.svg" alt="" class="icons1">
+                <img src="S.svg" alt="" class="icons2">
+                <img src="YM.svg" alt="" class="icons3">
+            </section>
+
+
+            <img src="" alt="">
+            <h1>Where words fail, music speaks</h1>
+            <h3>Music, the greatest discovery of mankind</h3>
+
+            <!-- <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab. Vero nostrum quis, odio veniam itaque ullam debitis qui magnam consequatur ab.</p> -->
+            <br>
+
+        </div>
+    </header>
+
+    <main>
+        <nav>
+            <button id="lyrics-btn">Imagin</button>
+            <button id="page2-btn">Can't take my eyes of you</button>
+            <button id="page3-btn">Fortunate son</button>
+        </nav>
+        <section id="content" style="color: white; display: flex;"></section>
+    </main>
+
+    <fieldset>
+        <div class="row">
+            <div class="column">
+
+                <p>
+
+                    Music is a moral law. It gives soul to the universe,<br>
+                    wings to the mind, flight to the imagination,<br>
+                    and charm and gaiety to life and to everything.
+
+                </p>
+
+            </div>
+
+
+            <div class="column">
+
+                <p>
+
+                    Music is a moral law. It gives soul to the universe,<br>
+                    wings to the mind, flight to the imagination,<br>
+                    and charm and gaiety to life and to everything.
+
+                </p>
+
+            </div>
+
+            <div class="column">
+
+                <p>
+
+                    “A great song should lift your heart,<br>
+                    warm the soul and make you feel good.”
+                </p>
+
+            </div>
+        </div>
+
+    </fieldset>
+
+    <footer style="
+    position: relative;
+    bottom: -34px;
+    left: 10%;
+    height: 7%;
+    width: 80%;
+    border-radius: 30px;
+    border-bottom: 1px solid;
+">
+        <p>&copy; Immerse yourself in music. All rights reserved.</p>
+    </footer>
+    <script src="front-page.js"></script>
+
+</body>
+
+</html>
